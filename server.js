e3f50e9818f557856ed77360e445b608bbcc9f55
Naman Shankhydhar <naman.geek@gmail.com>
2023-09-03 16:49:57 +0530

    feat: mongodb data source added

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const { mongoose } = require("mongoose");
 const app = express();
+
+const Event = require("./models/event");
 
 const PORT = process.env.PORT || 4000;
 
@@ -11,11 +14,25 @@ app.use(
     "/graphql",
     graphqlHTTP({
         schema: buildSchema(`
+        type Event{
+            _id:ID!
+            title:String!
+            description:String!
+            price:Float!
+            date:String
+        }
+
+        input EventInput{
+            title:String!
+            description:String!
+            price:Float!
+            date:String
+        }
         type RootQuery{
-            events:[String!]!
+            events:[Event!]!
         }
         type RootMutation{
-            createEvent(name:String):String
+            createEvent(eventType:EventInput):Event!
         }
         schema{
             query:RootQuery
@@ -23,14 +40,45 @@ app.use(
         }
     `),
         rootValue: {
-            events: () => ["Event 1", "Event 2", "Event 3"],
+            events: () => {
+                return Event.find().then((events) =>
+                    events.map((event) => {
+                        return {
+                            ...event._doc,
+                            _id: event._doc._id.toString(),
+                        };
+                    })
+                );
+            },
             createEvent: (args) => {
-                return args.name;
+                const { id, title, description, price, date } = args.eventType;
+                const event = new Event({
+                    title,
+                    description,
+                    price,
+                    date: new Date(date),
+                });
+                return event
+                    .save()
+                    .then((result) => {
+                        console.log(result);
+                        return { ...result._doc };
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        throw err;
+                    });
             },
         },
         graphiql: true,
     })
 );
-app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
-});
+
+mongoose
+    .connect(process.env.CONN_STRING)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started at ${PORT}`);
+        });
+    })
+    .catch((err) => console.log(err));
